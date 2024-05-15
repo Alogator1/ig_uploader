0cf5c3f2872d69b9126793016df73b035b467ac2
Vadym Telyachy <vtelyachy@gmail.com>
2024-05-15 12:27:27 -0500

    Added author of reels

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import Groq from 'groq-sdk';
 import { IgApiClient } from "instagram-private-api";
 import TelegramBot from "node-telegram-bot-api";
 import get from 'request-promise';
+import axios from 'axios';
 
 dotenv.config();
 
@@ -14,13 +15,15 @@ telegramBot.onText(/\/start/, (msg) => {
   telegramBot.sendMessage(msg.chat.id, `Welcome ${msg.chat.first_name}`);
 });
 
-telegramBot.onText(/instagram.com/, async (msg) => {
+telegramBot.onText(/instagram.com\/reel/, async (msg) => {
   if(msg.from.id == process.env.MY_ACCOUNT_ID) {
     const dataList = await instagramDl(msg.text);
     const ig = new IgApiClient();
 
     ig.state.generateDevice(process.env.IG_USERNAME);
     await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+
+    const author = await getInstagramReelAuthor(msg.text);
 
     await telegramBot.sendMessage(msg.chat.id, `Downloading ${JSON.stringify(dataList)} files...`);
 
@@ -53,7 +56,7 @@ telegramBot.onText(/instagram.com/, async (msg) => {
     await ig.publish.video({
       video,
       coverImage,
-      caption: `${res?.choices[0]?.message?.content}\n\n#MemeMonday #FunnyFridays #LOL #Humor #DailyLaughs #MemeMagic #ViralVibes #LaughOutLoud #ComedyGold #HilariousMoments #fyp #meme`,
+      caption: `${res?.choices[0]?.message?.content}\n\nStolen from: @${author}\n\n#MemeMonday #FunnyFridays #LOL #Humor #DailyLaughs #MemeMagic #ViralVibes #LaughOutLoud #ComedyGold #HilariousMoments #fyp #meme`,
     });
 
     await telegramBot.sendMessage(msg.chat.id, `Files uploaded!`);
@@ -61,3 +64,34 @@ telegramBot.onText(/instagram.com/, async (msg) => {
     telegramBot.sendMessage(msg.chat.id, `You are not allowed to use this bot.`);
   }
 });
+
+async function getInstagramReelAuthor(reelUrl) {
+  try {
+      const { data } = await axios.get(reelUrl);
+
+      // Extract the meta tag content
+      const metaTagRegex = /<meta property="og:description" content="([^"]+)"/;
+      const match = metaTagRegex.exec(data);
+
+      if (match && match[1]) {
+          const content = match[1];
+          // Extract the username using regex
+          const usernameRegex = /- ([^ ]+) on/;
+          const usernameMatch = usernameRegex.exec(content);
+
+          if (usernameMatch && usernameMatch[1]) {
+              const username = usernameMatch[1].trim();
+              return username;
+          } else {
+              console.error('Username not found in the meta tag content');
+              return null;
+          }
+      } else {
+          console.error('Meta tag not found');
+          return null;
+      }
+  } catch (error) {
+      console.error('Error fetching the URL:', error.message);
+      return null;
+  }
+}
